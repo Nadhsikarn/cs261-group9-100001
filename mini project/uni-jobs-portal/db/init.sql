8b47a0bea6be624cd9b0e8a17588e754bed96438
Itthiphon <itthiphon.jab@dome.tu.ac.th>
2025-11-13 00:02:58 +0700

    Create table 'notification' in database

--- mini project/uni-jobs-portal/db/init.sql
+++ mini project/uni-jobs-portal/db/init.sql
@@ -1,7 +1,7 @@
 -- 1️⃣ สร้าง database campusjobs (ถ้ายังไม่มี)
 IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = N'campusjobs')
 BEGIN
-    CREATE DATABASE campusjobs;
+    CREATE DATABASE campusjobs COLLATE Thai_CI_AS;
 END
 GO
 
@@ -9,10 +9,17 @@ GO
 USE campusjobs;
 GO
 
--- 3️⃣ สร้าง table [user] (8 columns)
+-- 3️⃣ สร้าง table [user] (8 columns) กับ table [noti]
 IF OBJECT_ID('dbo.[users]', 'U') IS NOT NULL
 BEGIN
     DROP TABLE dbo.[users];
+    DROP TABLE dbo.[notifications]
+END
+GO
+
+IF OBJECT_ID('dbo.[notifications]', 'U') IS NOT NULL
+BEGIN
+    DROP TABLE dbo.[notifications];
 END
 GO
 
@@ -26,6 +33,15 @@ CREATE TABLE dbo.[users] (
     [role] NVARCHAR(50) NOT NULL,
     [username] NVARCHAR(100) NOT NULL,
     [password] NVARCHAR(255) NULL
+)
+CREATE TABLE dbo.[notifications] (
+    [id] BIGINT IDENTITY(1,1) PRIMARY KEY,
+    [user_id] BIGINT NOT NULL,
+    [description] NVARCHAR(500) NULL,
+    [is_read] BIT NOT NULL DEFAULT 0,
+    [created_at] DATETIME2 NOT NULL DEFAULT SYSDATETIME(),
+    [link_url] NVARCHAR(500) NULL,
+    CONSTRAINT FK_notifications_users FOREIGN KEY([user_id]) REFERENCES dbo.[users]([id])
 );
 GO
 
