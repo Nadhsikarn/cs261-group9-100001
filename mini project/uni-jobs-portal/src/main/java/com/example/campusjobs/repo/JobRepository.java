8a15869b889a99ad25770cbd1e3a264d8cca975f
PawaritMangnimit <pawarit.man@dome.tu.ac.th>
2025-11-16 01:09:03 +0700

    Update JobRepository.java

--- mini project/uni-jobs-portal/src/main/java/com/example/campusjobs/repo/JobRepository.java
+++ mini project/uni-jobs-portal/src/main/java/com/example/campusjobs/repo/JobRepository.java
@@ -2,8 +2,14 @@ package com.example.campusjobs.repo;
 
 import com.example.campusjobs.model.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
 import java.util.List;
 
 public interface JobRepository extends JpaRepository<Job, Long> {
     List<Job> findByCreatorUsernameOrderByCreatedAtDesc(String creatorUsername);
+    int countByCreatorUsername(String creatorUsername);
+
+int countByCreatorUsernameAndCloseDateAfter(String creatorUsername, LocalDate today);
+    
 }
