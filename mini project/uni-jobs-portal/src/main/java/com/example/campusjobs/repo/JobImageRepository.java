de02c3a6ffe8f2c38ae5882fe3a24e1468d233ad
PawaritMangnimit <pawarit.man@dome.tu.ac.th>
2025-11-17 14:55:23 +0700

    fix edit image

--- mini project/uni-jobs-portal/src/main/java/com/example/campusjobs/repo/JobImageRepository.java
+++ mini project/uni-jobs-portal/src/main/java/com/example/campusjobs/repo/JobImageRepository.java
@@ -2,5 +2,11 @@ package com.example.campusjobs.repo;
 
 import com.example.campusjobs.model.JobImage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface JobImageRepository extends JpaRepository<JobImage, Long> {}
+public interface JobImageRepository extends JpaRepository<JobImage, Long> {
+    @Transactional
+    @Modifying
+    void deleteByJobId(Long jobId);
+}
